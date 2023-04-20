26989c5c8aecd0a64c66045f74bf76a620288899
Ethan Weiner <ethanweiner20@gmail.com>
2023-04-20 15:07:13 -0400

    chore: add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import SeamlessSection from '@/components/SeamlessSection';
 import HeroSection from '@/components/HeroSection';
 import ProjectsSection from '@/components/ProjectsSection';
+
 export default function Home() {
   return (
     <main className="mx-auto max-w-2xl px-4 sm:px-6 md:max-w-4xl ">
